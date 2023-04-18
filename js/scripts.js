76915f02844e9f2cb81f54d14ebaae5d77916681
Jason Falk <jasonfalk9@gmail.com>
2023-04-18 10:28:36 -0700

    Add test and business logic for if a user inputs a 0 in number.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Utility Logic
+// Business Logic
+function beepBoop(num) {
+  numHolderArray = [];
+  if (num === 0) {
+    numHolderArray.push(num);
+  }
+  console.log(numHolderArray, "numHolderArray");
+}
+beepBoop(0);
+//UI Logic
